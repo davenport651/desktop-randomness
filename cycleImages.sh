547b78babcc21824ee5a34135a083d7d666c080e
Davenport <41927748+davenport651@users.noreply.github.com>
2022-04-28 12:50:44 -0400

    Update cycleImages.sh
    updated home directory to the somewhat universal $HOME variable

--- cycleImages.sh
+++ cycleImages.sh
@@ -3,13 +3,13 @@
 #All files should have file names without spaces until further notice -mwd
 
 #Planets
-dir='/home/mdavenport/Pictures/planets/'
+dir='$HOME/Pictures/planets/'
 #file=`/bin/ls -1 "$dir" | sort --random-sort | head -1`
 file=`/bin/ls -p -R  "$dir" | grep -v /  | sort --random-sort | head -1`
 path=`readlink --canonicalize "$dir/$file"` # Converts to usable full path
 echo "The randomly-selected file is: $path"  # Echo full file path for debugging
 
-cp $path /home/mdavenport/Pictures/glPlanet
+cp $path '$HOME/Pictures/glPlanet'
 
 #echo $file
 #echo " "
@@ -19,7 +19,7 @@ file=""
 path=""
 
 #Ultrawide desktop
-dir='/home/mdavenport/Pictures/ultrawide/'
+dir='$HOME/Pictures/ultrawide/'
 #file=`/bin/ls -1 "$dir" | sort --random-sort | head -1`
 file=`/bin/ls -p -R  "$dir" | grep -v /  | sort --random-sort | head -1`
 path=`readlink --canonicalize "$dir/$file"` # Converts to usable full path
@@ -28,8 +28,8 @@ echo "The randomly-selected file is: $path"  # Echo full file path for debugging
 /bin/sleep 2
 pcmanfm --desktop-off
 /bin/sleep 2
-cp $path /home/mdavenport/Pictures/desktopultrawide
-nitrogen --set-auto /home/mdavenport/Pictures/desktopultrawide
+cp $path '$HOME/Pictures/desktopultrawide'
+nitrogen --set-auto '$HOME/Pictures/desktopultrawide'
 
 #if [[date +%F%t%T]-[uptime -s]]<"3:00" then xscreensaver-demo;
 #echo $file
